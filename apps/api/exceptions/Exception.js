41eec64eb8afd2c06a22a5f471fe572aad8dafe8
Pedro Medina <medinamarquezp@gmail.com>
2020-09-10 01:35:48 +0200

    Main exception created

---
+++ apps/api/exceptions/Exception.js
@@ -0,0 +1,8 @@
+class Exception extends Error {
+  constructor(statusCode, message) {
+    super();
+    this.statusCode = statusCode;
+    this.message = message;
+  }
+}
+module.exports = Exception;
