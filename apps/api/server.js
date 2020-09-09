2427c434cfb2a8d026429b6f8d985f97096f601c
Pedro Medina <medinamarquezp@gmail.com>
2020-09-10 01:41:45 +0200

    Added 4xx and 5xx error handler to API server

--- apps/api/server.js
+++ apps/api/server.js
@@ -2,13 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
 const port = process.env.PORT || 3000;
 
 const appRoutes = require("@routes/api");
+const Exception = require("@exceptions/Exception");
+const ErrorHandler = require("@handlers/ErrorHandler");
 
 app.use(bodyParser.json());
 app.use("/api", appRoutes);
+
+app.use("*", (req, res, next) => {
+  throw new Exception(404, "Not found");
+});
+
+app.use((err, req, res, next) => {
+  ErrorHandler.response(err, res);
+});
 
 app.listen(port, () => {
   console.log(`API server listening on port ${port}`);
