55f930c30cd62f02611037d3cd9ed3dedf8042f2
Pedro Medina <medinamarquezp@gmail.com>
2020-09-10 02:15:34 +0200

    Added missing params exception in mine block method

--- apps/api/controllers/BlockChainController.js
+++ apps/api/controllers/BlockChainController.js
@@ -1,4 +1,5 @@
 const BlockChainRepo = require("@repositories/InMemoryBlockChainRepo");
+const Exception = require("@exceptions/Exception");
 const ResponseHandler = require("@handlers/ResponseHandler");
 class BlockChainController {
   static getBlocks(req, res) {
@@ -8,6 +9,11 @@ class BlockChainController {
 
   static mineBlock(req, res) {
     const { data } = req.body;
+    if (!data)
+      throw new Exception(
+        400,
+        "data property is mandatory to perform this request"
+      );
     const block = BlockChainRepo.mineBlock(data);
     console.log(`New block added to chain: ${block}`);
     res.redirect("/api/blocks");
