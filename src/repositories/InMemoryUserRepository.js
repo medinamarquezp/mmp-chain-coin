2b195a00a3e9538fbf17ab05977cada6076fd483
Pedro Medina <medinamarquezp@gmail.com>
2020-09-18 23:56:11 +0200

    Created in memory user repository

---
+++ src/repositories/InMemoryUserRepository.js
@@ -0,0 +1,38 @@
+const User = require("@entities/User");
+const Hash = require("@services/Hash");
+
+class InMemoryUserRepository {
+  constructor() {
+    this.users = [];
+  }
+  async createUser(username, email, password) {
+    this.validateIdUserExists(email);
+    User.validatePassword(password);
+    const hashedPassword = await Hash.password(password);
+    const userCreated = new User(username, email, hashedPassword);
+    this.users.push(userCreated);
+    return userCreated.getUserToken();
+  }
+
+  async getUserToken(email, password) {
+    this.validateIfUserDoesNotExist(email);
+    const user = this.findUserByEmail(email);
+    const isPasswordCorrect = await Hash.compare(password, user.password);
+    if (!isPasswordCorrect) throw new Error("Incorrect password");
+    return user.getUserToken();
+  }
+
+  findUserByEmail(email) {
+    return this.users.find((user) => user.email === email);
+  }
+
+  validateIdUserExists(email) {
+    if (this.findUserByEmail(email)) throw new Error("User already exists");
+  }
+
+  validateIfUserDoesNotExist(email) {
+    if (!this.findUserByEmail(email)) throw new Error("User does not exists");
+  }
+}
+
+module.exports = InMemoryUserRepository;
---
+++ tests/repositories/InMemoryUserRepository.test.js
@@ -0,0 +1,43 @@
+const InMemoryUserRepository = require("@repositories/InMemoryUserRepository");
+describe("In memory user repository", () => {
+  let repo;
+  beforeEach(() => {
+    repo = new InMemoryUserRepository();
+  });
+  test("it should validate if user already exists on creating a new user", async () => {
+    const user = await repo.createUser("test", "test@test.es", "testASD123");
+    const createDuplicateUser = () =>
+      repo.createUser("test", "test@test.es", "testASD123");
+    await expect(createDuplicateUser()).rejects.toThrow("User already exists");
+  });
+  test("it should throw an error if password is not correct on creating user", async () => {
+    const createNewUserWithInvalidPassword = () =>
+      repo.createUser("test", "test@test.es", "test");
+    await expect(createNewUserWithInvalidPassword()).rejects.toThrow(
+      "Password must have at least 8 characters, letters and numbers [a-zA-Z0-9]"
+    );
+  });
+  test("it should create a new user", async () => {
+    await repo.createUser("test", "test@test.es", "testASD123");
+    const firstUserOnRepo = repo.users[0];
+    expect(firstUserOnRepo.email).toBe("test@test.es");
+  });
+  test("it should display an error on getting a token if user does not exist", async () => {
+    const getUserToken = () => repo.getUserToken("test@test.es", "test");
+    await expect(getUserToken()).rejects.toThrow("User does not exists");
+  });
+  test("it should display an error on getting a token if password is incorrect", async () => {
+    await repo.createUser("test", "test@test.es", "testASD123");
+    const getUserToken = () => repo.getUserToken("test@test.es", "test");
+    await expect(getUserToken()).rejects.toThrow("Incorrect password");
+  });
+  test("It should get a correct token", async () => {
+    const createdUserToken = await repo.createUser(
+      "test",
+      "test@test.es",
+      "testASD123"
+    );
+    const getUserToken = await repo.getUserToken("test@test.es", "testASD123");
+    expect(createdUserToken).toBe(getUserToken);
+  });
+});
