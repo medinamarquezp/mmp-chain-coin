78866bf0c93fe0b95d2ab560ada03fca66d73d33
Pedro Medina <medinamarquezp@gmail.com>
2020-09-19 22:38:05 +0200

    Auth validator created

---
+++ apps/api/validators/authValidator.js
@@ -0,0 +1,23 @@
+const { check } = require("express-validator");
+
+const signUpValidationRules = () => {
+  return [
+    check("username").notEmpty().withMessage("Username required"),
+    check("email")
+      .notEmpty()
+      .withMessage("Email required")
+      .isEmail()
+      .withMessage("Wrong email format"),
+    check("password")
+      .notEmpty()
+      .withMessage("Password property required")
+      .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,255}$/)
+      .withMessage(
+        "Password must have at least 8 characters, letters upper and lower case and numbers"
+      ),
+  ];
+};
+
+module.exports = {
+  signUpValidationRules,
+};
