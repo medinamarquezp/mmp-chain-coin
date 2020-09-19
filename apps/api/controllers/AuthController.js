4253268eb57e71c0b99b52fb9ae8dbe324cff29f
Pedro Medina <medinamarquezp@gmail.com>
2020-09-20 01:55:23 +0200

    Auth controller refactor

--- apps/api/controllers/AuthController.js
+++ apps/api/controllers/AuthController.js
@@ -1,17 +1,13 @@
-const { validationResult } = require("express-validator");
-const InMemoryUserRepository = require("@repositories/InMemoryUserRepository");
-const ResponseHandler = require("@handlers/ResponseHandler");
 const Exception = require("@exceptions/Exception");
+const { validationResult } = require("express-validator");
+const ResponseHandler = require("@handlers/ResponseHandler");
+const InMemoryUserRepository = require("@repositories/InMemoryUserRepository");
 
 class AuthController {
   static repo = new InMemoryUserRepository();
 
   static async signUp(req, res, next) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      const err = new Exception(422, errors.array());
-      return next(err);
-    }
+    AuthController.validateRequest(req);
     const { username, email, password } = req.body;
     let createdUser;
     try {
@@ -21,10 +17,22 @@ class AuthController {
         password
       );
     } catch (error) {
-      const err = new Exception(400, error.message);
-      return next(err);
+      AuthController.catchError(error);
     }
     ResponseHandler.response(res, createdUser);
+  }
+
+  static catchError(error) {
+    const err = new Exception(400, error.message);
+    return next(err);
+  }
+
+  static validateRequest(req) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const err = new Exception(422, errors.array());
+      return next(err);
+    }
   }
 }
 
