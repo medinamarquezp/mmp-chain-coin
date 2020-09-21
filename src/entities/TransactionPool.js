7247745dda4912e7c1511b4a36098bc8b5b410d9
Pedro Medina <medinamarquezp@gmail.com>
2020-09-21 13:41:16 +0200

    Get valid transactions from pool

--- src/entities/TransactionPool.js
+++ src/entities/TransactionPool.js
@@ -1,3 +1,5 @@
+const Transaction = require("@entities/Transaction");
+
 class TransactionPool {
   constructor() {
     this.transactions = [];
@@ -19,6 +21,26 @@ class TransactionPool {
 
   findTransactionByAddress(address) {
     return this.transactions.find((t) => t.input.address === address);
+  }
+
+  validTransactions() {
+    return this.transactions.filter((transaction) => {
+      const outputTotal = transaction.outputs.reduce((total, output) => {
+        return total + output.amount;
+      }, 0);
+      if (transaction.input.amount != outputTotal) {
+        console.error(`Invalid transaction from ${transaction.input.address}`);
+        return;
+      }
+      if (!Transaction.verifyTransaction(transaction)) {
+        console.error(`Invalid signature from ${transaction.input.address}`);
+        return;
+      }
+    });
+  }
+
+  clear() {
+    this.transactions = [];
   }
 }
 
