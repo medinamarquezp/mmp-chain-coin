19c1e796fa3696419b1b63bfe749bbbecbf774d6
Pedro Medina <medinamarquezp@gmail.com>
2020-09-23 19:44:10 +0200

    Added getters for reward and transactions list

--- src/entities/Miner.js
+++ src/entities/Miner.js
@@ -20,6 +20,28 @@ class Miner {
     this.p2pServer.broadcastClearTransaction();
     return block;
   }
+
+  static getTransactionsOutputs(minedBlock) {
+    return minedBlock.data.map((transaction) => transaction.outputs);
+  }
+
+  static rewardTransaction(minedBlock) {
+    const outputs = Miner.getTransactionsOutputs(minedBlock);
+    return outputs.slice(outputs.length - 1, outputs.length).flat()[0];
+  }
+
+  static transactionList(minedBlock) {
+    const outputs = Miner.getTransactionsOutputs(minedBlock);
+    const transactions = outputs.slice(0, outputs.length - 1);
+    let list = [];
+    transactions.forEach((transaction) =>
+      transaction.forEach((object, index) => {
+        index == 0 ? (object.action = "replace") : (object.action = "add");
+        list.push(object);
+      })
+    );
+    return list;
+  }
 }
 
 module.exports = Miner;
--- tests/entities/Miner.test.js
+++ tests/entities/Miner.test.js
@@ -9,23 +9,34 @@ describe("Miner entity test", () => {
   let wallet, blockChain, p2pServer, transactionPool, miner;
   beforeEach(() => {
     wallet = new Wallet();
+    wallet2 = new Wallet();
     blockChain = new BlockChain();
     transactionPool = new TransactionPool();
     p2pServer = new P2PServer(blockChain, transactionPool);
     wallet.createTransaction("ASDF1234QWERT", 40, transactionPool);
     wallet.createTransaction("MNBV0987YUIO", 10, transactionPool);
+    wallet2.createTransaction("MNBV0987YUIO", 50, transactionPool);
     miner = new Miner(blockChain, transactionPool, wallet, p2pServer);
   });
   test("It should mine transactions", () => {
     const minedBlock = miner.mine();
+    console.log(minedBlock);
     expect(transactionPool.transactions.length).toBe(0);
-    expect(minedBlock.data.length).toBe(2);
+    expect(minedBlock.data.length).toBe(3);
   });
   test("It should regard to miner when mine a new transaction", () => {
     const minerPublicKey = wallet.publicKey;
     const minedBlock = miner.mine();
-    const lastTransactionOutputs = minedBlock.data[1].outputs[0];
-    expect(lastTransactionOutputs.amount).toBe(MINING_REWARD);
-    expect(lastTransactionOutputs.address).toBe(minerPublicKey);
+    const rewardTransaction = Miner.rewardTransaction(minedBlock);
+    expect(rewardTransaction.amount).toBe(MINING_REWARD);
+    expect(rewardTransaction.address).toBe(minerPublicKey);
+  });
+  test("It should get transactions list without rewarded transaction", () => {
+    const minedBlock = miner.mine();
+    const transactionList = Miner.transactionList(minedBlock);
+    expect(transactionList.length).toBe(5);
+    expect(transactionList[0].action).toBe("replace");
+    expect(transactionList[1].action).toBe("add");
+    expect(transactionList[1].amount).toBe(40);
   });
 });
